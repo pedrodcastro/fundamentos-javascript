266bdc4e0b8365895c85d5f7d500c3a0920c6d74
Pedro Castro <contatoaugustopedro@gmail.com>
2023-03-28 15:38:18 -0300

    estudos sobre arrays no curso 2 de JavaScript

---
+++ curso2-arrays/atualizando-lista.js
@@ -0,0 +1,8 @@
+const listaDeChamada = ['João', 'Ana', 'Caio', 'Lara', 'Marjorie', 'Leo'];
+
+// listaDeChamada.splice(1, 2, 'Rodrigo');
+listaDeChamada.splice(2, 0, 'Rodrigo');
+//Posso utilizar esse método para adicionar algum elemento sem tirar nada;
+
+
+console.log(`Lita de chamada: ${listaDeChamada}`)
