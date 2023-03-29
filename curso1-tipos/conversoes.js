92377b80a3dae88f2fd6394c0f1e818168286c85
Pedro Castro <contatoaugustopedro@gmail.com>
2023-03-29 11:29:58 -0300

    Organizando as pastas do curso

---
+++ curso1-tipos/conversoes.js
@@ -0,0 +1,12 @@
+// tipo de dado
+// booleanos
+
+//coversão implicita
+const numero = 456;
+const numeroString = '456';
+
+console.log(numero + Number(numeroString));
+
+//conversão explicita
+//Number() --> converte em number
+//String() --> converte em string
---
+++ curso1-tipos/type-string.js
@@ -0,0 +1,23 @@
+const texto1 = "Olá mundo!";
+const texto2 = 'Olá mundo!';
+const senha = "senhaSuperSegura456!";
+const stringDeNumeros = "1247198247";
+
+const citacao = 'Meeu nome é: ';
+const meuNome = 'Pedro';
+
+//concatenação (operador de adição +)
+
+// console.log(citacao + meuNome);
+
+//template string ou template literal
+
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
